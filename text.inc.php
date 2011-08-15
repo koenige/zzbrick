b28d2bb842a23396c32115116213cfc958d3c64c
Gustaf Mossakowski <web@koenige.org>
2011-08-15 05:00:17 +0000

    brick_text() prüft nun, ob die letzte Variable ein gültiger Wert in 'brick_formatting_functions' ist, falls ja, wird diese Variable als Funktionsname genutzt, nicht als Text (falls Variablenname ausgegeben werden soll, nutze Anführungsstriche)
    git-svn-id: file:///Repos/zzbrick/trunk@55 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- text.inc.php
+++ text.inc.php
@@ -23,13 +23,22 @@ function brick_text($brick) {
 	// default: use wrap_text() from core/language.inc.php from zzwrap
 	if (!isset($brick['setting']['brick_translate_text_function']))
 		$brick['setting']['brick_translate_text_function'] = 'wrap_text';
+	if (!isset($brick['setting']['brick_formatting_functions']))
+		$brick['setting']['brick_formatting_functions'] = array();
 
 	// Translate text
 	if (!isset($brick['page']['text'][$brick['position']]))
 		$brick['page']['text'][$brick['position']] = '';
-	$brick['page']['text'][$brick['position']] 
-		.= $brick['setting']['brick_translate_text_function'](implode(' ', $brick['vars']));
-	unset ($brick['vars']);
+	$function = end($brick['vars']);
+	if (in_array($function, $brick['setting']['brick_formatting_functions'])) {
+		array_pop($brick['vars']);	
+	} else {
+		$function = false;
+	}
+	$text = $brick['setting']['brick_translate_text_function'](implode(' ', $brick['vars'])); 
+	if ($function) $text = $function($text);
+	$brick['page']['text'][$brick['position']] .= $text;
+	unset($brick['vars']);
 
 	return $brick;
 }
