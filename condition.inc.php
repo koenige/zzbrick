6145fad63d3afa4d3d6560751538e9cd14fb10dc
Gustaf Mossakowski <web@koenige.org>
2014-01-26 10:05:38 +0000

    zzbrick condition: added 'unless' next to if, endif, else and elseif
    git-svn-id: file:///Repos/zzbrick/trunk@121 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- condition.inc.php
+++ condition.inc.php
@@ -8,7 +8,7 @@
  * http://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2009-2012 Gustaf Mossakowski
+ * @copyright Copyright © 2009-2014 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -43,6 +43,7 @@ function brick_condition($brick) {
 	$brick['setting']['brick_condition_translated']['elseif'] = ':=';
 	$brick['setting']['brick_condition_translated']['else'] = ':';
 	$brick['setting']['brick_condition_translated']['endif'] = '-';
+	$brick['setting']['brick_condition_translated']['unless'] = '!';
 
 	if (in_array($brick['vars'][0], array_keys($brick['setting']['brick_condition_translated']))) {
 		$brick['vars'][0] = $brick['setting']['brick_condition_translated'][$brick['vars'][0]];
@@ -57,6 +58,9 @@ function brick_condition($brick) {
 		array_shift($brick['vars']);
 	} elseif ($brick['vars'][0] === ':=') {
 		$condition = ':=';
+		array_shift($brick['vars']);
+	} elseif ($brick['vars'][0] === '!') {
+		$condition = '!';
 		array_shift($brick['vars']);
 	} else {
 		$condition = '=';
@@ -140,6 +144,15 @@ function brick_condition($brick) {
 		unset($brick['condition_show'][$i]);
 		// reset show state to last condition if there is one
 		$i--;
+		break;
+	case '!':
+		if (!$show) break;
+		if (!$content) {
+			$brick['condition_content_shown'][$i] = true;
+		} else {
+			$brick['condition_content_shown'][$i] = false;
+			$show = false;
+		}
 		break;
 	}
 	
