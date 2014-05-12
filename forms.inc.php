79b20416f02163321447ee73f71cdcf693c05738
Gustaf Mossakowski <web@koenige.org>
2014-05-12 12:16:09 +0000

    zzbrick forms now includes an editinc.inc.php in custom/zzform directory to allow the separation of output functions and functions which are only needed if the database content will be changed
    git-svn-id: file:///Repos/zzbrick/trunk@144 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- forms.inc.php
+++ forms.inc.php
@@ -117,6 +117,9 @@ function brick_forms($brick) {
 		AND file_exists($brick['tables_path'].'/_common.inc.php')) {
 		include_once $brick['tables_path'].'/_common.inc.php';
 	}
+	if (file_exists($path = $zz_conf['custom_dir'].'/editing.inc.php')) {
+		include_once $path;
+	}
 
 	// script path must be first variable
 	$brick = brick_forms_file($brick);
