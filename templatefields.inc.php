490280f9788dec2e5b56e5c5ce4699a408fe4dd5
Gustaf Mossakowski <web@koenige.org>
2015-05-20 10:37:18 +0000

    zzbrick templatefields: for debugging purposes, prints out a list of fields that are available in the template fo putting into the template header; prints out some real values as well; default module gets a template for that
    git-svn-id: file:///Repos/zzbrick/trunk@184 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ templatefields.inc.php
@@ -0,0 +1,48 @@
+<?php 
+
+/**
+ * zzbrick
+ * Show all fields available in template
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/projects/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2015 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * show all fields available in template
+ * 
+ * files: -
+ * functions: -
+ * settings: -
+ * examples:
+ *		%%% templatefields %%%
+ * @param array $brick	Array from zzbrick
+ * @return array $brick
+ */
+function brick_templatefields($brick) {
+	$fields = brick_templatefields_recursive($brick['parameter']);
+	$text = wrap_template('templatefields', $fields);
+	echo 'This is for debugging purposes only.<br><br># List of fields:<br>';
+	echo '# '.$text;
+	echo '<br><br>Real content:<br>';
+	echo wrap_print($brick['parameter']);
+	exit;
+}
+
+function brick_templatefields_recursive($values) {
+	$i = 0;
+	foreach ($values as $key => $value) {
+		$keys[$i]['key'] = $key;
+		if (is_array($value)) {
+			$values = reset($value);
+			$keys[$i]['subkeys'] = brick_templatefields_recursive($values);
+		}
+		$i++;
+	}
+	return $keys;
+}
