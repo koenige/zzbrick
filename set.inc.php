53872349a7764c1c9ec93c3d984779a523c72623
Gustaf Mossakowski <gustaf@koenige.org>
2020-03-23 11:13:49 +0000

    add 'set' to change settings, currently only set content_type=txt (or similar) is possible
    git-svn-id: file:///Repos/zzbrick/trunk@263 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ set.inc.php
@@ -0,0 +1,36 @@
+<?php 
+
+/**
+ * zzbrick
+ * change settings
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/projects/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * change settings for page
+ * 
+ * examples: 
+ * 		%%% set content_type=txt %%% 
+ * @param array $brick
+ * @return array $brick
+ */
+function brick_set($brick) {
+	if (empty($brick['vars'][0])) return $brick;
+	$brick = brick_local_settings($brick);
+	if (empty($brick['local_settings'])) return $brick;
+	
+	$overwrite_allow = ['content_type'];
+	foreach (array_keys($brick['local_settings']) as $key) {
+		if (!in_array($key, $overwrite_allow)) unset ($brick['local_settings'][$key]);
+	}
+	
+	$brick['page'] = array_merge($brick['page'], $brick['local_settings']);
+	return $brick;
+}
