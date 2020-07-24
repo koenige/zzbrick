4cfaa5649fcbba6118d67c970c3cdfe91fe37b1e
Gustaf Mossakowski <gustaf@koenige.org>
2020-07-24 10:10:08 +0000

    forms: only set 'active_module' if it is not already set
    git-svn-id: file:///Repos/zzbrick/trunk@271 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- forms.inc.php
+++ forms.inc.php
@@ -340,14 +340,16 @@ function brick_forms_file($brick) {
 		$module_path = $brick['setting']['modules_dir'].'/'.$module.$brick['module_path'];
 		$brick['form_script_path'] = $module_path.'/'.$script.'.php';
 		if (file_exists($brick['form_script_path'])) {
-			$brick['setting']['active_module'] = $module;
+			if (empty($brick['setting']['active_module']))
+				$brick['setting']['active_module'] = $module;
 			return $brick;
 		}
 		if ($module !== 'default') continue;
 		// default-module has some database_-prefixed tables
 		$brick['form_script_path'] = $module_path.'/database_'.$script.'.php';	
 		if (file_exists($brick['form_script_path'])) {
-			$brick['setting']['active_module'] = $module;
+			if (empty($brick['setting']['active_module']))
+				$brick['setting']['active_module'] = $module;
 			return $brick;
 		}
 	}
