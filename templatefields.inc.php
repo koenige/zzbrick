79379260a48a2c5c7c79561768298b997b7ea8cf
Gustaf Mossakowski <gustaf@koenige.org>
2020-12-18 20:40:59 +0000

    templatefields: initialize $keys
    git-svn-id: file:///Repos/zzbrick/trunk@286 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- templatefields.inc.php
+++ templatefields.inc.php
@@ -8,7 +8,7 @@
  * http://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2019 Gustaf Mossakowski
+ * @copyright Copyright © 2015, 2019-2020 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -36,7 +36,8 @@ function brick_templatefields($brick) {
 
 function brick_templatefields_recursive($values) {
 	$i = 0;
-	if (!is_array($values)) return [];
+	$keys = [];
+	if (!is_array($values)) return $keys;
 	$first = key($values);
 	if (is_numeric($first)) {
 		$values = reset($values);
