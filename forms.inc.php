7eb8c9b715de543d006a0686819a3a163a3db613
Gustaf Mossakowski <gustaf@koenige.org>
2021-02-22 12:49:07 +0000

    forms: if 'html_fragment' is set, return JSON with HTML fragment, title, url instead of pure HTML fragment
    git-svn-id: file:///Repos/zzbrick/trunk@294 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- forms.inc.php
+++ forms.inc.php
@@ -195,7 +195,12 @@ function brick_forms($brick) {
 	$zz_conf['show_output'] = false;
 	$ops = zzform($zz);
 	if (!empty($ops['html_fragment'])) {
-		$brick['page']['template'] = 'empty';
+		$output = [
+			'html' => $ops['output'],
+			'title' => $ops['title'],
+			'url' => $ops['redirect_url']
+		];
+		return wrap_send_text(json_encode($output), 'json', 200);
 	}
 	
 	// Map? Only in list-mode and if there are records
