1037bfb757b1563f1b2940d42e94a8763bb860f0
Gustaf Mossakowski <gustaf@koenige.org>
2023-01-28 10:58:22 +0000

    libraries: add zzbrick include script to include zzbrick.php automatically as a module
    git-svn-id: file:///Repos/zzbrick/trunk@385 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ libraries/zzbrick.inc.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * zzbrick
+ * include zzbrick as a library into zzwrap
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/projects/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+include __DIR__.'/../zzbrick/zzbrick.php';
