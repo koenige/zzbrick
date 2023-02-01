2d538754369cf914172ae759b7994dafbedcee58
Gustaf Mossakowski <gustaf@koenige.org>
2023-02-01 12:01:14 +0000

    path: if second parameter is set, try to read value from loop_parameter or parameter key with second parameter as key
    git-svn-id: file:///Repos/zzbrick/trunk@390 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/path.inc.php
+++ zzbrick/path.inc.php
@@ -4,7 +4,7 @@
  * zzbrick
  * path
  *
- * Part of »Zugzwang Project«
+ * Part of ÈZugzwang ProjectÇ
  * http://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
@@ -27,20 +27,28 @@
  */
 function brick_path($brick) {
 	$text = '';
+	if (count($brick['vars']) > 1)
+		if (is_array($brick['loop_parameter']) AND array_key_exists($brick['vars'][1], $brick['loop_parameter']))
+			$parameter = $brick['loop_parameter'][$brick['vars'][1]];
+		elseif (is_array($brick['parameter']) AND array_key_exists($brick['vars'][1], $brick['parameter']))
+			$parameter = $brick['parameter'][$brick['vars'][1]];
+		else
+			$parameter = '';
+
 	switch (count($brick['vars'])) {
 		case 1:
 			$text = wrap_path($brick['vars'][0]);
 			break;
 		case 2:
-			$text = wrap_path($brick['vars'][0], $brick['vars'][1]);
+			$text = wrap_path($brick['vars'][0], $parameter);
 			break;
 		case 3:
 			if ($brick['vars'][3] === 'check_rights=0')
-				$text = wrap_path($brick['vars'][0], $brick['vars'][1], false);
+				$text = wrap_path($brick['vars'][0], $parameter, false);
 			elseif ($brick['vars'][3] === 'check_rights=1')
-				$text = wrap_path($brick['vars'][0], $brick['vars'][1], true);
+				$text = wrap_path($brick['vars'][0], $parameter, true);
 			else
-				$text = wrap_path($brick['vars'][0], $brick['vars'][1]);
+				$text = wrap_path($brick['vars'][0], $parameter);
 			break;
 	}
 	$brick['page']['text'][$brick['position']][] = $text;
