128915dead78057d66ca57745c65c894cc694e58
Gustaf Mossakowski <gustaf@koenige.org>
2024-03-15 15:56:14 +0000

    text: allow local settings, e. g. context=, send this as variable to wrap_text(); allow values-variable for wrap_text() with item if text is only one word but in quotes, too
    git-svn-id: file:///Repos/zzbrick/trunk@453 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/text.inc.php
+++ zzbrick/text.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2009, 2014, 2019, 2021-2023 Gustaf Mossakowski
+ * @copyright Copyright © 2009, 2014, 2019, 2021-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -23,11 +23,12 @@
  * 		%%% text hello %%% 
  * 		%%% text We like to use our CMS! %%%
  * 		%%% text "We found %d items" item_count %%%
+ * 		%%% text "We found %d items" context=msgctxt %%%
  * @param array $brick
  * @return array $brick
  */
 function brick_text($brick) {
-	// Translate text
+	$brick = brick_local_settings($brick);
 	if (!isset($brick['page']['text'][$brick['position']]))
 		$brick['page']['text'][$brick['position']] = [];
 	$function = end($brick['vars']);
@@ -37,7 +38,7 @@ function brick_text($brick) {
 	} else {
 		$function = false;
 	}
-	if (count($brick['vars']) > 1 AND strstr($brick['vars'][0], ' ')) {
+	if (count($brick['vars']) > 1 AND (strstr($brick['vars'][0], ' ') OR !empty($brick['in_quotes']))) {
 		$text = array_shift($brick['vars']);
 		$sprintf_params = $brick['vars'];
 	} else {
@@ -52,7 +53,10 @@ function brick_text($brick) {
 			$sprintf_params += $matches[2];
 		}
 	}
-	
+	$text_params = [];
+	if (!empty($brick['local_settings']['context']))
+		$text_params['context'] = $brick['local_settings']['context']; 
+
 	if ($sprintf_params) {
 		if (!empty($brick['loop_parameter'])) {
 			$item = &$brick['loop_parameter'];
@@ -75,9 +79,10 @@ function brick_text($brick) {
 		if ($function)
 			foreach ($params as $index => $param)
 				$params[$index] = $function($param);
-		$text = wrap_text($text, ['values' => $params]);
+		$text_params['values'] = $params;
+		$text = wrap_text($text, $text_params);
 	} else {
-		$text = wrap_text($text);
+		$text = wrap_text($text, $text_params);
 		if ($function) $text = $function($text);
 	}
 	
