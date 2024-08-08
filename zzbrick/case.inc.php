4e17b551827f673359b2f11ccc57367f756fdb94
Gustaf Mossakowski <gustaf@koenige.org>
2024-08-08 11:42:37 +0000

    switch/case: add switch and case statements to display text depending on value of field
    git-svn-id: file:///Repos/zzbrick/trunk@487 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick/case.inc.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+ * zzbrick
+ * switch/case
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/projects/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * switch case construct, case part
+ * 
+ * files: -
+ * functions: -
+ * settings: -
+ * examples: 
+ * 		%%% switch field %%%
+ *		%%% case value1 %%% content shown if field = value1
+ *		%%% case value2 %%% content shown if field = value2
+ * 		%%% switch end %%% 
+ * @param array $brick
+ * @return array $brick
+ */
+function brick_case($brick) {
+	if (count($brick['vars']) !== 1) return $brick;
+
+	if ($brick['position'] === '_hidden_') {
+		if ($brick['vars'][0] === $brick['switch'])
+			$brick['position'] = $brick['position_old'];
+	} else {
+		if ($brick['vars'][0] !== $brick['switch'])
+			$brick['position'] = '_hidden_';
+	}
+
+	return $brick;
+}
---
+++ zzbrick/switch.inc.php
@@ -0,0 +1,46 @@
+<?php 
+
+/**
+ * zzbrick
+ * switch/case
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/projects/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * switch case construct, switch part
+ * 
+ * files: -
+ * functions: -
+ * settings: -
+ * examples: 
+ * 		%%% switch field %%%
+ *		%%% case value1 %%% content shown if field = value1
+ *		%%% case value2 %%% content shown if field = value2
+ * 		%%% switch end %%% 
+ * @param array $brick
+ * @return array $brick
+ */
+function brick_switch($brick) {
+	if (count($brick['vars']) !== 1) return $brick;
+
+	switch ($brick['vars'][0]) {
+		case 'end':
+			$brick['switch'] = NULL;
+			$brick['position'] = $brick['position_old'];
+			$brick['position_old'] = NULL;
+			break;
+		default:
+			$brick['switch'] = $brick['parameter'][$brick['vars'][0]] ?? NULL;
+			$brick['position_old'] = $brick['position'];
+			break;
+	}
+	
+	return $brick;
+}
