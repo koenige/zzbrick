835f04824d7ce470183cff9759c990193bee895c
Gustaf Mossakowski <gustaf@koenige.org>
2024-10-31 23:54:44 +0000

    icon: search for icons in upload media folder, too; remove XML declaration if it exists
    git-svn-id: file:///Repos/zzbrick/trunk@498 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/icon.inc.php
+++ zzbrick/icon.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2019, 2023 Gustaf Mossakowski
+ * @copyright Copyright © 2015, 2019, 2023-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -28,12 +28,14 @@ function brick_icon($brick) {
 	if (count($brick['vars']) !== 1) return '';
 	
 	$icon = sprintf('icons/%s.svg', $brick['vars'][0]);
-	$files = wrap_collect_files($icon, 'custom/modules/themes');
+	$files = wrap_collect_files($icon, 'media/custom/modules/themes');
 	if (!$files) {
 		$content = '?';
 	} else {
 		$file = reset($files);
 		$content = file_get_contents($file);
+		if (strstr($content, '<?xml version="1.0" encoding="UTF-8"?>'))
+			$content = str_replace('<?xml version="1.0" encoding="UTF-8"?>', '', $content);
 	}
 
 	$brick['page']['text'][$brick['position']][] = $content;
