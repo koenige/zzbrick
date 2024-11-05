f9c61bf45f42f3642849aea87d71bd0542e5868e
Gustaf Mossakowski <gustaf@koenige.org>
2024-11-05 12:04:28 +0000

    icon: fix header
    git-svn-id: file:///Repos/zzbrick/trunk@501 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/icon.inc.php
+++ zzbrick/icon.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/projects/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2019, 2023-2024 Gustaf Mossakowski
+ * @copyright Copyright © 2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
