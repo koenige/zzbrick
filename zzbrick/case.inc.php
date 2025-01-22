48f07dfd86ecf19f2c45636d2682206b1ca8a0e9
Gustaf Mossakowski <gustaf@koenige.org>
2025-01-22 12:25:32 +0000

    switch: add `default` (or `case :`) for default handling of variable
    git-svn-id: file:///Repos/zzbrick/trunk@516 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/case.inc.php
+++ zzbrick/case.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -23,6 +23,8 @@
  * 		%%% switch field %%%
  *		%%% case value1 %%% content shown if field = value1
  *		%%% case value2 %%% content shown if field = value2
+ *		%%% case : %%% content shown in other cases or
+ *		%%% default %%% content shown in other cases
  * 		%%% switch end %%% 
  * @param array $brick
  * @return array $brick
@@ -32,6 +34,15 @@ function brick_case($brick) {
 
 	if ($brick['vars'][0] === $brick['switch']) {
 		$brick['position'] = $brick['position_switch'];
+		$brick['switch_used'][] = brick_case_id($brick);
+	} elseif ($brick['vars'][0] === ':' OR $brick['vars'][0] === 'default') {
+		if (in_array(brick_case_id($brick), $brick['switch_used'])) {
+			$brick['position'] = '_hidden_';
+			// initialize text at _hidden_ position
+			$brick['page']['text'][$brick['position']] = [];
+		} else {
+			$brick['position'] = $brick['position_switch'];
+		}
 	} else {
 		$brick['position'] = '_hidden_';
 		// initialize text at _hidden_ position
@@ -39,4 +50,8 @@ function brick_case($brick) {
 	}
 
 	return $brick;
+}
+
+function brick_case_id($brick) {
+	return sprintf('%s:%s', $brick['switch_field'], $brick['switch']);
 }
--- zzbrick/switch.inc.php
+++ zzbrick/switch.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -23,6 +23,7 @@
  * 		%%% switch field %%%
  *		%%% case value1 %%% content shown if field = value1
  *		%%% case value2 %%% content shown if field = value2
+ *		%%% default %%% content shown in other cases
  * 		%%% switch end %%% 
  * @param array $brick
  * @return array $brick
@@ -36,11 +37,14 @@ function brick_switch($brick) {
 			$brick['switch'] = NULL;
 			$brick['position'] = $brick['position_switch'];
 			$brick['position_switch'] = NULL;
+			$brick['switch_field'] = NULL;
 			break;
 		default:
+			$brick['switch_used'] = [];
 			$brick['switch'] = $brick['parameter'][$brick['vars'][0]]
 				?? $brick['loop_parameter'][$brick['vars'][0]] ?? NULL;
 			$brick['position_switch'] = $brick['position'];
+			$brick['switch_field'] = $brick['vars'][0];
 			break;
 	}
 	
