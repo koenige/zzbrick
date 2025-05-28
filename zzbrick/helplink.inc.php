02e3212337ea0336cfda4a5cd3b85b43a6f2206c
Gustaf Mossakowski <gustaf@koenige.org>
2025-05-28 16:21:28 +0000

    helplink: add possibility to add link to help text, if it is found
    git-svn-id: file:///Repos/zzbrick/trunk@530 98be6e6d-ea4f-4117-9388-ed847deb6ed9

---
+++ zzbrick/helplink.inc.php
@@ -0,0 +1,34 @@
+<?php 
+
+/**
+ * zzbrick
+ * link to help text
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/zzbrick
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2025 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * link to help text
+ * 
+ * examples: 
+ * 		%%% helplink Name of the help text %%% 
+ * @param array $brick
+ * @return array $brick
+ */
+function brick_helplink($brick) {
+	$filename = implode('-', $brick['vars']);
+	$filename = strtolower($filename);
+
+	wrap_include('request', 'zzbrick');
+	$data = brick_request_data('helptexts', [$filename]);
+	if (!$data) return $brick;
+
+	$brick['page']['text'][$brick['position']][] = wrap_template('helplink', $data);
+	return $brick;
+}
