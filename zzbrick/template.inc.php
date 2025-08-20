d8e993f8b540a5b63b7a24aa5beab6f860409410
Gustaf Mossakowski <gustaf@koenige.org>
2025-08-20 14:22:06 +0000

    template: support using parameters key=value which are passed to template as values; do not return with empty string if more than one variable is given but with $brick
    git-svn-id: file:///Repos/zzbrick/trunk@537 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/template.inc.php
+++ zzbrick/template.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2019, 2023 Gustaf Mossakowski
+ * @copyright Copyright © 2015, 2019, 2023, 2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -21,13 +21,20 @@
  * settings: -
  * examples: 
  * 		%%% template name-of-template %%% 
+ * 		%%% template name-of-template key=value %%% 
  * @param array $brick
  * @return array $brick
  */
 function brick_template($brick) {
-	if (count($brick['vars']) !== 1) return '';
+	$brick = brick_local_settings($brick);
 
-	$brick['page']['text'][$brick['position']][] = wrap_template($brick['vars'][0]);
+	if (count($brick['vars']) !== 1) {
+		unset($brick['vars']);
+		return $brick;
+	}
+
+	$brick['page']['text'][$brick['position']][]
+		= wrap_template($brick['vars'][0], $brick['local_settings'] ?? []);
 	unset($brick['vars']);
 
 	return $brick;
