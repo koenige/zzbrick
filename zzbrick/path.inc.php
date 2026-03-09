471aca217b835b903c3d1bef6da7c17fff5a537e
Gustaf Mossakowski <gustaf@koenige.org>
2026-03-09 23:49:09 +0000

    path: add support for setting key + value (and more settings), not only exactly one setting
    git-svn-id: file:///Repos/zzbrick/trunk@582 98be6e6d-ea4f-4117-9388-ed847deb6ed9

--- zzbrick/path.inc.php
+++ zzbrick/path.inc.php
@@ -22,6 +22,7 @@
  * examples:
  *		%%% path area %%%
  *		%%% path area value %%%
+ *		%%% path area setting setting_key value %%%
  *		%%% path area value check_rights=0 html="<a href='%s'>" %%%
  * @param array $brick	Array from zzbrick
  * @return array $brick
@@ -38,16 +39,20 @@ function brick_path($brick) {
 
 	// get values
 	$values = [];
-	if (count($brick['vars']) === 2 AND $brick['vars'][0] === 'setting') {
-		$values[] = wrap_setting($brick['vars'][1]);
-	} else {
-		foreach ($brick['vars'] as $var)
-			if (is_array($brick['loop_parameter']) AND array_key_exists($var, $brick['loop_parameter']))
-				$values[] = $brick['loop_parameter'][$var];
-			elseif (is_array($brick['parameter']) AND array_key_exists($var, $brick['parameter']))
-				$values[] = $brick['parameter'][$var];
-			else
-				$values[] = $var;
+	$i = 0;
+	while ($i < count($brick['vars'])) {
+		$var = $brick['vars'][$i];
+		if ($var === 'setting' AND $i + 1 < count($brick['vars'])) {
+			$i++;
+			$values[] = wrap_setting($brick['vars'][$i]);
+		} elseif (is_array($brick['loop_parameter']) AND array_key_exists($var, $brick['loop_parameter'])) {
+			$values[] = $brick['loop_parameter'][$var];
+		} elseif (is_array($brick['parameter']) AND array_key_exists($var, $brick['parameter'])) {
+			$values[] = $brick['parameter'][$var];
+		} else {
+			$values[] = $var;
+		}
+		$i++;
 	}
 
 	$testing = (!$values and !empty($brick['parameter']['brick_condition_if'])) ? true : false;
