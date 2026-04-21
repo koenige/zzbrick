f078bc2dff92e2f8c9b03230be592c783d46f8cb
Gustaf Mossakowski <gustaf@koenige.org>
2026-04-21 07:50:10 +0200

    icon: add support for reading the filename from a field value via `field=some_field`; fix svg role

--- zzbrick/icon.inc.php
+++ zzbrick/icon.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/zzbrick
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -20,14 +20,25 @@
  * functions: -
  * settings: -
  * examples: 
- * 		%%% icon name-of-icon %%% 
+ * 		%%% icon name-of-icon %%%
+ * 		%%% icon field=some_field %%% (value of some_field will be used as filename)
  * @param array $brick
  * @return array $brick
  */
 function brick_icon($brick) {
-	if (count($brick['vars']) !== 1) return '';
+	$brick = brick_local_settings($brick);
 	
-	$icon = sprintf('icons/%s.svg', $brick['vars'][0]);
+	if (!$brick['vars'] AND !empty($brick['local_settings']['field'])) {
+		if (empty($brick['loop_parameter'][$brick['local_settings']['field']]))
+			return $brick;
+		$filename = $brick['loop_parameter'][$brick['local_settings']['field']];
+	} elseif (count($brick['vars']) !== 1) {
+		return $brick;
+	} else {
+		$filename = $brick['vars'][0];
+	}
+	
+	$icon = sprintf('icons/%s.svg', $filename);
 	$files = wrap_collect_files($icon, 'files/custom/modules/themes');
 	if (!$files) {
 		$content = '?';
@@ -39,7 +50,7 @@ function brick_icon($brick) {
 		$title = basename($file);
 		if (strstr($title, '.'))
 			$title = substr($title, 0, strpos($title, '.'));
-		if (!strstr('role="img"', $content))
+		if (!strstr($content, 'role="img"'))
 			$content = str_replace('<svg', '<svg role="img" aria-label="'.$title.'"', $content);
 		$content = '<span class="svg-title">'.$title.'</span>'.$content;
 	}
